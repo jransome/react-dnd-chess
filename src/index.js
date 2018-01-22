cfc1dd7ab948d90e940b54d4ae09b4d75a53e6ba
jransome <jamesransome@msn.com>
2018-01-22 16:22:35 +0000

    add square component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Square from './components/square'
 import Knight from './components/knight'
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Knight />, document.getElementById('root'));
+ReactDOM.render(
+    <Square black={true}>
+        <Knight />
+    </Square>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
